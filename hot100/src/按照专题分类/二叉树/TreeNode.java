338c2ebbb880c8e242f00e09e997263317c7f373
syh <85404658+su322@users.noreply.github.com>
2025-08-14 16:58:51 +0800

    update

---
+++ hot100/src/按照专题分类/二叉树/TreeNode.java
@@ -0,0 +1,20 @@
+package 按照专题分类.二叉树;
+
+// 自己要会写哦
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
